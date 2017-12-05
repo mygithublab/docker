70fa5b1a1208d5a662312ea2584fcfc0c884a6cf
mygithublab <mygithublab@126.com>
2017-12-06 00:34:51 +0800

    20171206

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-/etc/init.d/sshd start && /etc/init.d/nagios start && /etc/init.d/httpd start && /sbin/service xinetd restart && /bin/bash
+
+/etc/init.d/sshd start && /etc/init.d/nagios start && /etc/init.d/httpd start && /bin/bash
 
