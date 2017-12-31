76c4ffd62ea84c89a74df7f6373b098389322000
mygithublab <mygithublab@126.com>
2017-12-31 15:33:20 +0800

    20171231

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+DOCKER_IMAGE_NAME=nagios
+DOCKER_CONTAINER_NAME=nagiosgraph
+
+docker build -t "${DOCKER_RUN_IMAGE}" .
+
+docker images
+
+docker run -itd --name "${DOCKER_CONTAINER_NAME}" -p 4000:80 -p 4001:22 -v /mnt:/mnt -t "${DOCKER_RUN_IMAGE}"
+
+docker ps -a
