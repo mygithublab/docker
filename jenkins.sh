5be28f4d6584375ad8a0dfbd669c00bbef3471f5
mygithublab <mygithublab@126.com>
2018-04-21 23:28:11 +0800

    20180421232811

--- jenkins.sh
+++ jenkins.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker build -t mygithublab . \
-#&& docker rm -f mygithublab \
-&& docker run -itd -p 8000:80 -p 8001:22 --name mygithublab -v /mnt:/mnt --restart=always mygithublab
+docker build -t mygithublab . 
+#docker rm -f mygithublab 
+docker run -itd -p 8000:80 -p 8001:22 --name mygithublab -v /mnt:/mnt --restart=always mygithublab
